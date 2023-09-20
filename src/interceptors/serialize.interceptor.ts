546a65175194a504dd501a8b33ef2c65d7b9753b
Efstratios Bektasiadis <mpektastratos1997@gmail.com>
2023-09-20 16:57:41 +0300

    implemented serialize interceptor

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+import { ClassConstructor, plainToInstance } from 'class-transformer';
+
+export function Serialize(dto: ClassConstructor<unknown>) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: ClassConstructor<unknown>) {}
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
