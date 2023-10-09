66bd66250c63dbe9403e7bef0c2fabdb0713cb4b
Efstratios Bektasiadis <ebektasiadis@gmail.com>
2023-10-09 13:26:41 +0300

    added initial migration & config

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -2,22 +2,25 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 export let dataSourceOptions: DataSourceOptions;
 
+dataSourceOptions = {
+  migrations: ['migrations/*.js'],
+  synchronize: false,
+} as DataSourceOptions;
+
 if (process.env.NODE_ENVIRONMENT === 'development') {
-  dataSourceOptions = {
+  dataSourceOptions = Object.assign(dataSourceOptions, {
     type: 'sqlite',
     database: 'db.sqlite',
     entities: ['**/*.entity.js'],
-    synchronize: true,
-  };
+  });
 }
 
 if (process.env.NODE_ENVIRONMENT === 'test') {
-  dataSourceOptions = {
+  dataSourceOptions = Object.assign(dataSourceOptions, {
     type: 'sqlite',
     database: 'testdb.sqlite',
     entities: ['**/*.entity.ts'],
-    synchronize: true,
-  };
+  });
 }
 
 const dataSource = new DataSource(dataSourceOptions);
